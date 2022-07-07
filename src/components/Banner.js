b92816e92aa28f6ba140febfbc5ae5c251355f28
Flavio Coutinho <coutinhoflavio20@gmail.com>
2022-07-07 19:56:04 -0300

    Add multi-carousel

---
+++ src/components/Banner.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Software Developer🚀"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        },delta)
+
+        return () => { clearInterval(ticker) };
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true)
+            setDelta(period);
+        } else if(isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+    }
+}
+
+    return (
+        <section className="banner" id="home">
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Bem Vindo ao meu Portfolio</span>
+                        <h1>{`Sou o Flávio Coutinho, `} <span className="wrap">{text}</span></h1>
+                        <p>PHP é podre, Flutter é Wordpress Mobile, Microsoft melhor empresa do mundo, JavaScript é podre</p>
+                        <button onAbort={() => console.log('connect')}>Entre em contacto <ArrowRightCircle size={25} /></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerImg} alt="Header Img" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
